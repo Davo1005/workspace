507183f5d38c3bc5bfce610421108c5eac14727d
Davo1005 <Davit.ghazaryan2005@gmail.com>
2025-02-08 19:01:24 +0400

    initial commit

---
+++ area.cpp
@@ -0,0 +1,21 @@
+double area ( double r)
+{
+	return 3.14*r;
+}
+
+double area ( double barc, double erk, char r)
+{
+        return barc*erk;
+}
+
+double area ( double himq, double barc)
+{
+        return (himq*barc)/2;
+}
+
+int area(int side)
+{
+	return side*side;
+}
+
+
---
+++ include.h
@@ -0,0 +1,6 @@
+#include <iostream>
+double area ( double r);
+double area ( double barc, double erk, char r);
+double area ( double himq, double barc);
+int area(int side);
+
